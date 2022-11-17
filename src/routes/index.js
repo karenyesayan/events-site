c1eb013fda5082574edabab6dc056f694b29f4c2
Karen-555 <karenyesayan0@gmail.com>
2022-11-17 19:26:01 +0400

    17/11

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
+import {lazy} from "react";
+
 import App from "../App";
 import Header from "../components/Header";
 
-import Home from "../pages/Home";
-import Cinema from "../pages/Cinema";
-import Concert from "../pages/Concert";
-import Theater from "../pages/Theater";
-import Other from "../pages/Other";
-import OperaBallet from "../pages/OperaBallet";
-import ClubsPubs from "../pages/ClubsPubs";
-
-import Account from "../pages/Account";
+const Home = lazy(() => import("../pages/Home"))
+const Cinema = lazy(() => import("../pages/Cinema"))
+const Concert = lazy(() => import("../pages/Concert"))
+const Theater = lazy(() => import("../pages/Theater"))
+const Other = lazy(() => import("../pages/Other"))
+const OperaBallet = lazy(() => import("../pages/OperaBallet"))
+const ClubsPubs = lazy(() => import("../pages/ClubsPubs"))
+const Account = lazy(() => import("../pages/Account"))
 
 
 const routes = [
