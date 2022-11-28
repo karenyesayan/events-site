768c6991edd1e2aba8c5d20981ab50221350ed53
MeriNikoghosyan <mnikoghosyan87@gmail.com>
2022-11-29 03:35:15 +0400

    29.11.22

--- src/Pages/Ticket/index.js
+++ src/Pages/Ticket/index.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
@@ -7,15 +8,22 @@ import { userSelector } from "../../redux/slices/userSlice"
 const Ticket = (navigateTo) => {
     const navigate = useNavigate();
     const user = useSelector(userSelector);
+    
+    useEffect(() => {
+        setTimeout(() => {
+            navigate('/profile')
+        },3000)
+        
+    },)
 
 
     return (
 
-        <div> hello {user.name}
-            <h1>this is your ticket </h1>
-            <img src={"https://d3jmn01ri1fzgl.cloudfront.net/photoadking/webp_thumbnail/5fe02b5de8d23_json_image_1608526685.webp"} className="upcoming-event-img" />
+        <div> dear {user.name}
+            <h1>This is your e-ticket</h1>
+            <img src={"https://media.istockphoto.com/id/1130968355/vector/cinema-ticket-on-white-background-movie-ticket-template-in-black-and-red-colors.jpg?s=612x612&w=is&k=20&c=xARjInKjIx4A1_z8wjltWtC_XXH9e5v0fQzQaSWdVPQ="} className="upcoming-event-img" />
         </div>
-
+       
     )
 
 }
--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,25 +1,34 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 
 import { userSelector } from "../../redux/slices/userSlice";
 
-
 const Profile = ({ navigateTo }) => {
 
-    const user = useSelector(userSelector);
+    const navigate = useNavigate();
+
+    const {name,email,ewallet} = useSelector(userSelector);
 
     return (
-        <div className={"upcoming"}>
-            <h5>Hello {user.name} this is your profile
-            </h5>
-            <h2> Your E-wallet balance is <b>: </b>
-                {user.ewallet}
-            </h2>
+
+        <div className="profile">
+            <img src={"http://tse3.mm.bing.net/th/id/OIP.vpU_KUELPRjvDl4PvY0xIAHaHa?w=179&h=180&c=7&r=0&o=5&pid=1.7"} className="profile-img" />
+            <p className="profile-title">
+                {name}
+            </p>
+            <p className="profile-title">
+                {email}
+            </p>
+            <p className="profile-title">
+                Your E-wallet balance is <b>: </b> {ewallet} dr
+            </p>
+            <div className="profil-user">
+                <center>
+                    <span className="profile-ticket-title">Your tickets </span>
+                </center>
+            </div>
         </div>
-
-
-
-
     )
-
 }
 export default Profile;
