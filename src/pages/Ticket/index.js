c142a4b145a1f42d41bfe21805ca89ffeed8c461
Karen-555 <karenyesayan0@gmail.com>
2022-12-09 13:58:00 +0400

    09/12

--- src/pages/Ticket/index.js
+++ src/pages/Ticket/index.js
@@ -3,13 +3,11 @@ import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 
 import { userSelector } from "../../redux/slices/userSlice"
-import { movieSelector } from "../../redux/slices/moviesSlice";
 
 
 const Ticket = (navigateTo) => {
     const navigate = useNavigate();
     const user = useSelector(userSelector);
-    const activMovie = useSelector(movieSelector);
     
     useEffect(() => {
         setTimeout(() => {
@@ -21,8 +19,8 @@ const Ticket = (navigateTo) => {
 
     return (
 
-        <div className="label"> dear {user.name}
-            <h1>e-ticket {activMovie.title}</h1>
+        <div> dear {user.name}
+            <h1>This is your e-ticket</h1>
             <img src={"https://media.istockphoto.com/id/1130968355/vector/cinema-ticket-on-white-background-movie-ticket-template-in-black-and-red-colors.jpg?s=612x612&w=is&k=20&c=xARjInKjIx4A1_z8wjltWtC_XXH9e5v0fQzQaSWdVPQ="} className="upcoming-event-img" />
         </div>
        
